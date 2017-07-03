bad7e4a0d3b9428ca9b40e7d0d03649367df98bf
Grant Surratt <gsurratt@gmail.com>
2017-07-03 12:53:52 -0700

    Fix 404

--- wrappers/md.jsx
+++ wrappers/md.jsx
@@ -4,26 +4,17 @@ import Helmet from 'react-helmet';
 import { config } from 'config';
 
 import Post from '../components/Post';
-import Error from '../components/Error';
 import Footer from '../components/Footer';
 
 class MarkdownWrapper extends React.Component {
   render() {
     const { route } = this.props;
     const post = route.page.data;
-    const layout = post.layout;
-    let template;
-
-    if (layout === 'notation' || layout == 'theme' || layout === 'page') {
-      template = <Post {...this.props} />;
-    } else if (layout === 'error') {
-      template = <Error {...this.props} />;
-    }
 
     return (
       <div>
         <Helmet title={`${config.siteTitle} - ${post.title}`} />
-        {template}
+        <Post {...this.props} />
         <Footer />
       </div>
     );
