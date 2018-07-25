6353dc3ed43156c532a0be504dcbaf15aa40c6a0
Grant Surratt <gsurratt@gmail.com>
2018-07-25 13:46:14 -0700

    set up org -> md script; move scripts & org symlinks/dirs

---
+++ scripts/export-org-to-md.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+find pages/notations/src -iname "*.org" -type f -exec sh -c 'pandoc -f org -t gfm -o "pages/notations/$(basename ${0} .org).md" ${0}' {} \;
+find pages/notations -iname "*.md" -type f -exec sh -c 'sed -i "" "1d;8d;" ${0}' {} \;
