82475c2c99fa8df4995bd2a69fb5a733898a827d
Grant Surratt <gsurratt@gmail.com>
2018-07-25 15:08:55 -0700

    make pandoc verbose

--- scripts/export-org-to-md.sh
+++ scripts/export-org-to-md.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-find pages/notations/src -iname "*.org" -type f -exec sh -c 'pandoc -f org -t gfm -o "pages/notations/$(basename ${0} .org).md" ${0}' {} \;
+find pages/notations/src -iname "*.org" -type f -exec sh -c 'pandoc --verbose -f org -t gfm -o "pages/notations/$(basename ${0} .org).md" ${0}' {} \;
 find pages/notations -iname "*.md" -type f -exec sh -c 'sed -i "" "1d;8d;" ${0}' {} \;
 
-find pages/_drafts/src -iname "*.org" -type f -exec sh -c 'pandoc -f org -t gfm -o "pages/_drafts/$(basename ${0} .org).md" ${0}' {} \;
+find pages/_drafts/src -iname "*.org" -type f -exec sh -c 'pandoc --verbose -f org -t gfm -o "pages/_drafts/$(basename ${0} .org).md" ${0}' {} \;
 find pages/_drafts -iname "*.md" -type f -exec sh -c 'sed -i "" "1d;8d;" ${0}' {} \;
